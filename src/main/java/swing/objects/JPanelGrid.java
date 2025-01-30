8ac2a566c78075d040cf7de1b0fb92eaf3265467
true-meowmeow <135vbn@gmail.com>
2025-01-31 00:08:08 +0400

    Я обновил аудиосистему за 3 тысячи

---
+++ src/main/java/swing/objects/JPanelGrid.java
@@ -0,0 +1,12 @@
+package swing.objects;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JPanelGrid extends JPanel {
+    public JPanelGrid() {
+        setLayout(new GridBagLayout());
+
+        //setBorder(BorderFactory.createLineBorder(Color.GREEN));
+    }
+}
