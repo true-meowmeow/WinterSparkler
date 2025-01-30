f064e02f7fe41560ed989d75f2eff8b9184af71d
true-meowmeow <135vbn@gmail.com>
2025-01-31 00:08:08 +0400

    Я обновил аудиосистему за 3 тысячи

---
+++ src/main/java/swing/objects/JPanelBorder.java
@@ -0,0 +1,13 @@
+package swing.objects;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JPanelBorder extends JPanel {
+
+    public JPanelBorder() {
+        setLayout(new BorderLayout());
+
+        setBorder(BorderFactory.createLineBorder(Color.GREEN));
+    }
+}
