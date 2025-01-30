bae25b5c4617f3a266b3b75dee6c36c276e218d9
true-meowmeow <135vbn@gmail.com>
2025-01-31 00:31:22 +0400

    Ура

--- src/main/java/swing/pages/home/PageHome.java
+++ src/main/java/swing/pages/home/PageHome.java
@@ -7,25 +7,14 @@ import java.awt.*;
 import static core.Methods.newGridBagConstraintsX;
 
 
-public class PageHome extends JPanelGrid {      //todo изменить структуру, надо разделить первые два на JPanel и разделить их, с 3 и 4 также, чтобы они были через другую Jpanel по парам
+public class PageHome extends JPanelGrid {
 
-    private final double weight1 = 0.19;
-    private final double weight2 = 0.19;
-    private final double weight3 = 0.42;
-    private final double weight4 = 0.2;
+
 
     public PageHome() {
 
-        setLayout(new GridBagLayout());
-
-/*        addPanel(0, weight1, new ArtistsPanel());
-        addPanel(1, weight2, new CollectionsPanel());
-        addPanel(2, weight3, new PlayPanel());
-        addPanel(3, weight4, new PlayPanel());*/
-
-        add(new LeftPageHome(), newGridBagConstraintsX(0, 0.38));
-        add(new RightPageHome(), newGridBagConstraintsX(1, 0.62));
-
+        add(new LeftPageHome(), newGridBagConstraintsX(0, 38));
+        add(new RightPageHome(), newGridBagConstraintsX(1, 62));
     }
 
 
@@ -34,8 +23,8 @@ public class PageHome extends JPanelGrid {      //todo изменить стру
 class LeftPageHome extends JPanelGrid {
 
     public LeftPageHome() {
-        add(new ArtistsPanel(), newGridBagConstraintsX(0, 0.5));
-        add(new CollectionsPanel(), newGridBagConstraintsX(1, 0.5));
+        add(new ArtistsPanel(), newGridBagConstraintsX(0, 50));
+        add(new CollectionsPanel(), newGridBagConstraintsX(1, 50));
 
     }
 }
@@ -43,8 +32,7 @@ class LeftPageHome extends JPanelGrid {
 class RightPageHome extends JPanelGrid {
 
     public RightPageHome() {
-        add(new PlayPanel(), newGridBagConstraintsX(0, 0.7));
-        add(new PlayPanel(), newGridBagConstraintsX(1, 0.3));
-
+        add(new PlayPanel(), newGridBagConstraintsX(0, 70));
+        add(new QueuePanel(), newGridBagConstraintsX(1, 30));
     }
 }
