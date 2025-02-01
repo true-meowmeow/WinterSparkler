2ce5484e1d7feb48c45211677a7522cc59d2f6de
true-meowmeow <135vbn@gmail.com>
2025-02-01 20:26:09 +0400

    Ура

--- src/main/java/swing/pages/home/PageHome.java
+++ src/main/java/swing/pages/home/PageHome.java
@@ -2,8 +2,6 @@ package swing.pages.home;
 
 import swing.objects.JPanelGrid;
 
-import java.awt.*;
-
 import static core.Methods.newGridBagConstraintsX;
 
 
@@ -13,25 +11,25 @@ public class PageHome extends JPanelGrid {
 
     public PageHome() {
 
-        add(new LeftPageHome(), newGridBagConstraintsX(0, 38));
-        add(new RightPageHome(), newGridBagConstraintsX(1, 62));
+        add(new CollectionPageHome(), newGridBagConstraintsX(0, 38));
+        add(new PlayerPageHome(), newGridBagConstraintsX(1, 62));
     }
 
 
 }
 
-class LeftPageHome extends JPanelGrid {
+class CollectionPageHome extends JPanelGrid {
 
-    public LeftPageHome() {
+    public CollectionPageHome() {
         add(new ArtistsPanel(), newGridBagConstraintsX(0, 50));
         add(new CollectionsPanel(), newGridBagConstraintsX(1, 50));
 
     }
 }
 
-class RightPageHome extends JPanelGrid {
+class PlayerPageHome extends JPanelGrid {
 
-    public RightPageHome() {
+    public PlayerPageHome() {
         add(new PlayPanel(), newGridBagConstraintsX(0, 70));
         add(new QueuePanel(), newGridBagConstraintsX(1, 30));
     }
