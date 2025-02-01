6f0c385edd8bd06957d67f934e6dc672e93b4b1d
true-meowmeow <135vbn@gmail.com>
2025-02-01 22:48:51 +0400

    Ура

--- src/main/java/swing/pages/home/queue/QueuePanel.java
+++ src/main/java/swing/pages/home/queue/QueuePanel.java
@@ -6,29 +6,18 @@ import javax.swing.*;
 import java.awt.*;
 
 import static core.Methods.createBorder;
-import static core.Methods.createVerticalPanel;
 
 public class QueuePanel extends JPanelBorder {
 
     public QueuePanel() {
-
-
-
-        // Центральная панель — ДОБАВЛЯЕМ ПУСТОЙ КОМПОНЕНТ ДЛЯ РАСТЯЖКИ
+        super(true);
         JPanel contentPanel = new JPanel(new BorderLayout());
-        createBorder(contentPanel);
+
         contentPanel.setBackground(Color.LIGHT_GRAY);
         contentPanel.add(new JLabel(), BorderLayout.CENTER); // Пустой компонент
         add(contentPanel, BorderLayout.CENTER);
 
-        JPanel bottomPanel = createVerticalPanel(250);
-
-        createBorder(bottomPanel);
-        add(bottomPanel, BorderLayout.SOUTH);
-        //huinya(bottomPanel);
-
-        // УБИРАЕМ ЛЮБЫЕ ФИКСИРОВАННЫЕ РАЗМЕРЫ У ПАНЕЛИ
-        setMinimumSize(new Dimension(0, 0));
         setPreferredSize(new Dimension(0, 0));
+        setMinimumSize(new Dimension(0, 0));
     }
 }
