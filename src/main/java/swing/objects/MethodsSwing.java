0e4c2e57d10d41ab3ef77a315ba23df96af3d6fe
true-meowmeow <135vbn@gmail.com>
2025-02-02 23:37:57 +0400

    верхняя панель шевелится

---
+++ src/main/java/swing/objects/MethodsSwing.java
@@ -0,0 +1,74 @@
+package swing.objects;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public class MethodsSwing {
+
+    public static void createBorder(JPanel jPanel) {
+        Random random = new Random();
+        Color randomColor;
+
+        do {
+            int r = random.nextInt(156) + 100; // 100–255
+            int g = random.nextInt(156) + 100;
+            int b = random.nextInt(156) + 100;
+
+            randomColor = new Color(r, g, b);
+        } while (isGrayish(randomColor)); // Проверяем, не получился ли серый
+
+        jPanel.setBorder(BorderFactory.createLineBorder(randomColor, 1));
+    }
+
+    private static boolean isGrayish(Color color) {
+        int r = color.getRed();
+        int g = color.getGreen();
+        int b = color.getBlue();
+
+        // Разница между каналами должна быть значительной (иначе цвет серый)
+        return (Math.abs(r - g) < 50) && (Math.abs(r - b) < 50) && (Math.abs(g - b) < 50);
+    }
+
+
+    public static GridBagConstraints newGridBagConstraintsX(int gridX, double weightX) {
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = gridX;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = weightX;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+
+        return gbc;
+    }
+
+    public static JTextArea createTextArea(String name) {
+        JTextArea textArea = new JTextArea();
+        textArea.setText(name);
+        textArea.setLineWrap(false);
+        textArea.setBorder(null);
+        textArea.setOpaque(false);
+        textArea.setWrapStyleWord(false);
+        textArea.setMinimumSize(new Dimension(0, 0));
+        textArea.setColumns(1); // Фиксированное количество символов        //todo отработать если текстовое поле пустое, наверное стоит вставлять что-то пустое типо null
+        textArea.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    e.consume(); // Блокируем нажатие Enter
+                    textArea.getParent().requestFocusInWindow(); // Убираем фокус
+                }
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) { //todo Обработать отмену и комбинацию ctrl + z одинаково, если не было нажат enter, то не сохраняет.
+                    e.consume(); // Блокируем нажатие Enter
+                    textArea.getParent().requestFocusInWindow(); // Убираем фокус
+                }
+            }
+        });
+        return textArea;
+    }
+}
