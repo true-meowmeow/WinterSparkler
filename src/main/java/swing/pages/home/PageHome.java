52064f3cb6e2df238061b462edda149a3f744d8a
meowmeow <meowmeow>
2025-02-03 00:10:52 +0400

    всё выше

--- src/main/java/swing/pages/home/PageHome.java
+++ src/main/java/swing/pages/home/PageHome.java
@@ -1,6 +1,6 @@
 package swing.pages.home;
 
-import swing.objects.JPanelGrid;
+import swing.objects.JPanelCustom;
 import swing.pages.home.collections.CollectionsFrame;
 import swing.pages.home.play.PlayFrame;
 import swing.pages.home.queue.QueueFrame;
@@ -9,27 +9,29 @@ import swing.pages.home.series.SeriesFrame;
 import static swing.objects.MethodsSwing.newGridBagConstraintsX;
 
 
-public class PageHome extends JPanelGrid {
+public class PageHome extends JPanelCustom {
 
     public PageHome() {
-        super(true);
+        super(PanelType.GRID, true);
         add(new CollectionPageHome(), newGridBagConstraintsX(0, 37));
         add(new PlayerPageHome(), newGridBagConstraintsX(1, 63));
     }
 }
 
-class CollectionPageHome extends JPanelGrid {
+class CollectionPageHome extends JPanelCustom {
 
     public CollectionPageHome() {
+        super(PanelType.GRID);
         add(new CollectionsFrame(), newGridBagConstraintsX(0, 50));
         add(new SeriesFrame(), newGridBagConstraintsX(1, 50));
 
     }
 }
 
-class PlayerPageHome extends JPanelGrid {
+class PlayerPageHome extends JPanelCustom {
 
     public PlayerPageHome() {
+        super(PanelType.GRID);
         add(new PlayFrame(), newGridBagConstraintsX(0, 75));
         add(new QueueFrame(), newGridBagConstraintsX(1, 25));
     }
