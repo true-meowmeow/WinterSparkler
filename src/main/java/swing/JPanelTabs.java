aed91d3280e1b2af2b843d48f27edff10df18bc7
meowmeow <meowmeow>
2025-02-05 01:05:46 +0400

    обработка настроек

--- src/main/java/swing/JPanelTabs.java
+++ src/main/java/swing/JPanelTabs.java
@@ -2,28 +2,27 @@ package swing;
 
 import swing.pages.Favorites;
 import swing.pages.PageComponent;
+import swing.pages.PageSettings; // Добавляем импорт
 import swing.pages.home.PageHome;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class JPanelTabs extends JPanel {
-
-
     CardLayout cardLayout = new CardLayout();
 
     PageHome pageHome = new PageHome();
     PageComponent pageComponent = new PageComponent();
     Favorites favorites = new Favorites();
-
+    PageSettings pageSettings = new PageSettings(); // Добавляем страницу настроек
 
     public JPanelTabs() {
         setVisible(true);
-
         setLayout(cardLayout);
+
         add(pageHome, "PageHome");
         add(pageComponent, "PageComponent");
         add(favorites, "Favorites");
-
+        add(pageSettings, "Settings"); // Добавляем в CardLayout
     }
 }
