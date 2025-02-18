991ef9b8a184fdb4e11b5cbfef4e02afd31805a4
true-meowmeow <135vbn@gmail.com>
2025-02-18 16:30:16 +0400

    рефактор говна

--- src/main/java/swing/pages/home/play/FolderSystemPanel.java
+++ src/main/java/swing/pages/home/play/FolderSystemPanel.java
@@ -3,35 +3,34 @@ package swing.pages.home.play;
 import core.contentManager.FilesData;
 import core.contentManager.FilesDataList;
 import swing.objects.JPanelCustom;
-import swing.objects.ScrollablePanel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
-import java.util.List;
 
 import static swing.pages.home.play.objects.FolderUtil.getChildFolders;
-import static swing.pages.home.play.objects.FolderUtil.removeTrailingSlash;
 
 public class FolderSystemPanel extends JPanelCustom {
 
     private String currentRoot = null;
-    private ScrollablePanel contentPanel; // Основной контейнер для панелей
+    //private ScrollablePanel contentPanel; // Основной контейнер для панелей
+    private JScrollPane scrollPane;
+
 
     public FolderSystemPanel(FilesDataList filesDataList) {
         super(PanelType.BORDER);
 
         // Создаем основной контейнер с вертикальным BoxLayout
-        contentPanel = new ScrollablePanel();
-        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        //contentPanel = new ScrollablePanel();
+        //contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
 
-        // Помещаем contentPanel в JScrollPane
-        JScrollPane scrollPane = new JScrollPane(contentPanel);
+
+        scrollPane = new JScrollPane(cardPanel);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         add(scrollPane, BorderLayout.CENTER);
 
-        updateAudioFiles(new FilesDataList());
+        //updateAudioFiles(new FilesDataList());
 
         // Добавляем тестовые панели
         //contentPanel.add(corePanel, BorderLayout.CENTER);
@@ -40,8 +39,75 @@ public class FolderSystemPanel extends JPanelCustom {
     String currentFolder;
     JPanelCustom cardPanel = new JPanelCustom(PanelType.CARD);      //todo изменить все имена в картах чтобы принимали путь корня чтобы избежать ошибки при одинаковом названии папок в разных корнях
 
+    public void updateManagingPanel(FilesDataList filesDataList) {
+        //contentPanel.removeAll();
+        cardPanel.removeAll();  //Каждая карта - все фильтрованные файлы по выбранной папке
+
+        JPanelCustom panel = new JPanelCustom(PanelType.BORDER, "Y");
+        for (int i = 0; i < filesDataList.getFilesDataListFiltered().size(); i++) { //Проходит по корневым путям
+            //panel.add(corePanel(filesDataList.getFilesDataListFiltered().get(i)));
+            //panel.add(initCardPanel(filesDataList.getFilesDataListFiltered().get(i)));
+
+
+            //Проходит по папкам
+            for (String path : filesDataList.getFilesDataListFiltered().get(i).getFullPathsHashSet()) {
+                //System.out.println(path);
+                cardPanel.add(initCardPanel(filesDataList.getFilesDataListFiltered().get(i)), path);
+
+            }
+
+
+
+        }
+
+
+    }
+
+
+    private JPanelCustom initCardPanel(FilesData filesData) {
+        JPanelCustom panel = new JPanelCustom(PanelType.BORDER, "Y");
+
+
+        //System.out.println(filesData);
+/*        panel.add(titlePanel(filesData.getRootPath()), BorderLayout.NORTH);
+
+        // Получаем подпапки для текущего пути (currentFolder)
+        String[] childFolders = getChildFolders(currentFolder, filesData.getRelativePathsHashSet());
+        panel.add(createFoldersPanel(childFolders));
+
+        // Добавляем карты для всех нужных путей
+        for (String path : filesData.getRelativePathsHashSet()) {
+            if (path.equals("")) {
+                continue;
+            }
+            cardPanel.add(innerFolders111(filesData, path), path);
+        }*/
+
+        return panel;
+    }
+
+    private JPanelCustom innerFolders111(FilesData filesData, String folderPath) {
+
+        //todo Добавить кнопку назад и пути перехода назад. + отслеживать fullPath
+        JPanelCustom panel = new JPanelCustom(PanelType.BORDER, "Y");
+        panel.add(new Label(folderPath));
+        //System.out.println(folderPath);
+
+/*        System.out.println(folderPath);
+        System.out.println(Arrays.toString(new TreeSet[]{filesData.getPathsHashSet()}));
+        System.out.println(Arrays.toString(getChildFolders(folderPath, filesData.getPathsHashSet())));
+        System.out.println();*/
+
+
+        panel.add(createFoldersPanel(
+                getChildFolders(folderPath, filesData.getRelativePathsHashSet()))
+        );
+
+        return panel;
+    }
+
     public void updateAudioFiles(FilesDataList filesDataList) {
-        contentPanel.removeAll();
+        //contentPanel.removeAll();
         cardPanel.removeAll();
         currentFolder = new String("");
 
@@ -56,9 +122,9 @@ public class FolderSystemPanel extends JPanelCustom {
 
         showCard("coreMainWinterSparkler");
         //showCard("тест пустой папки/вот эту папку надо/");
-        contentPanel.add(cardPanel);
-        contentPanel.revalidate();
-        contentPanel.repaint();
+        //contentPanel.add(cardPanel);
+        //contentPanel.revalidate();
+        //contentPanel.repaint();
     }
 
     public void showCard(String path) {
@@ -78,11 +144,11 @@ public class FolderSystemPanel extends JPanelCustom {
         panel.add(titlePanel(filesData.getRootPath()), BorderLayout.NORTH);
 
         // Получаем подпапки для текущего пути (currentFolder)
-        String[] childFolders = getChildFolders(currentFolder, filesData.getPathsHashSet());
+        String[] childFolders = getChildFolders(currentFolder, filesData.getRelativePathsHashSet());
         panel.add(createFoldersPanel(childFolders));
 
         // Добавляем карты для всех нужных путей
-        for (String path : filesData.getPathsHashSet()) {
+        for (String path : filesData.getRelativePathsHashSet()) {
             if (path.equals("")) {
                 continue;
             }
@@ -106,7 +172,7 @@ public class FolderSystemPanel extends JPanelCustom {
 
 
         panel.add(createFoldersPanel(
-                getChildFolders(folderPath, filesData.getPathsHashSet()))
+                getChildFolders(folderPath, filesData.getRelativePathsHashSet()))
         );
 
         return panel;
@@ -128,7 +194,6 @@ public class FolderSystemPanel extends JPanelCustom {
             foldersPanel.add(new FolderPanel(folder, defaultIconPath, this.cardPanel, fullPath));
         }
         //System.out.println();
-
 
 
         panel.add(foldersPanel, BorderLayout.CENTER);
