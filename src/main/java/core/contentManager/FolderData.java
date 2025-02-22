450e345d062d2f03ed30d5e28b60a602a8b0c7eb
true-meowmeow <135vbn@gmail.com>
2025-02-23 02:24:27 +0400

    блять голова болит от комплексности

--- src/main/java/core/contentManager/FolderData.java
+++ src/main/java/core/contentManager/FolderData.java
@@ -1,12 +1,58 @@
 package core.contentManager;
 
-public class FolderData {
+import java.util.Comparator;
 
+public class FolderData implements Comparable<FolderData>  {
 
+    Comparator<FolderData> rootPathComparator = Comparator.comparing(FolderData::getRootPath);
 
+    private String fullPath;
+    private String rootPath;
+    private String relativePath;
 
+    private boolean active = true; //Переключается на false при соответствии условии (Нет медиафайлов и одна папка)
 
+    private String name;            //folder name
 
+    public FolderData(String fullPath, String rootPath, String relativePath) {
+        this.fullPath = fullPath;
+        this.rootPath = rootPath;
+        this.relativePath = relativePath;
 
+        System.out.println(fullPath);
 
+        this.name = name;   //todo метод установки имени.
+    }
+
+    @Override
+    public int compareTo(FolderData o) {
+        return this.fullPath.compareTo(o.fullPath);
+    }
+
+    @Override
+    public String toString() {
+        return "FolderData{" +
+                "fullPath='" + fullPath + '\'' +
+                '}';
+    }
+
+    public String getFullPath() {
+        return fullPath;
+    }
+
+    public String getRootPath() {
+        return rootPath;
+    }
+
+    public String getRelativePath() {
+        return relativePath;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void deactivate() {
+        this.active = false;
+    }
 }
