af3bff02571deffd84d8ed2b7efc8f80004a1476
meowmeow <135vbn@gmail.com>
2025-04-06 00:59:00 +0400

    Ну что за код! Всё ярче этот свет!

--- src/main/java/swing/objects/selection/FolderPanel.java
+++ src/main/java/swing/objects/selection/FolderPanel.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class FolderPanel extends SelectablePanel {
 
     public FolderPanel(int index, String name) {
-        super(index, name, new Dimension(160, 80));  //70 70
+        super(index, name, new Dimension(70, 70));  //70 70
         setFolderActive();
     }
 }
