ab3456be474843d651f1546980c202994740ddc5
meowmeow <135vbn@gmail.com>
2025-04-14 23:04:28 +0400

    Где мой веер? Где мой лог?

--- src/main/java/swing/objects/selection/DropTargetSeries.java
+++ src/main/java/swing/objects/selection/DropTargetSeries.java
@@ -4,17 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
-public class DropTargetPanel extends JPanel {
+public class DropTargetSeries extends DropPanelChild {
     private JLabel infoLabel;
 
-    public DropTargetPanel() {
+    public DropTargetSeries() {
         setLayout(new BorderLayout());
         setBackground(Color.WHITE);
         infoLabel = new JLabel("Drop items here", SwingConstants.CENTER);
         add(infoLabel, BorderLayout.CENTER);
     }
 
-    public void dropItems(List<SelectablePanel> items) {
+    public void dropItems(java.util.List<SelectablePanel> items) {
         StringBuilder namesList = new StringBuilder();
         for (SelectablePanel sp : items) {
             namesList.append(sp.getDisplayText()).append(", ");
@@ -25,5 +25,12 @@ public class DropTargetPanel extends JPanel {
         String info = "Dropped: [" + namesList + "] (Count: " + items.size() + ")";
         infoLabel.setText(info);
         System.out.println(info);
+    }
+
+    @Override
+    public void action(List<SelectablePanel> selectedItems) {
+        super.action(selectedItems);
+        dropItems(selectedItems);
+        System.out.println("2222222222");
     }
 }
