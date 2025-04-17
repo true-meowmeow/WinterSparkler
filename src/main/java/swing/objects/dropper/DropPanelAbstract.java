79f4e2de6b193f538ae9ef6e874f0d827eb96824
meowmeow <135vbn@gmail.com>
2025-04-18 00:54:11 +0400

    Пусть этот фикс живёт миллион времён!
    Signed-off-by: meowmeow <47986831+true-meowmeow@users.noreply.github.com>

---
+++ src/main/java/swing/objects/dropper/DropPanelAbstract.java
@@ -0,0 +1,15 @@
+package swing.objects.dropper;
+
+import swing.objects.general.JPanelCustom;
+import swing.objects.selection.SelectablePanel;
+
+import java.util.List;
+
+public abstract class DropPanelAbstract extends JPanelCustom {
+
+    public void dropAction(List<SelectablePanel> selectedItems) {
+    }
+
+    public void dropAction() {
+    }
+}
