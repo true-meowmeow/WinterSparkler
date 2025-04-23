5faf8eefe8020b99319af466e3fb9eff0f47abf6
meowmeow <47986831+true-meowmeow@users.noreply.github.com>
2025-04-23 05:47:57 +0400

    На ветке dev танцуем до утра!

--- src/main/java/swing/objects/general/JPanelCustom.java
+++ src/main/java/swing/objects/general/JPanelCustom.java
@@ -1,256 +1,74 @@
 package swing.objects.general;
 
-import swing.objects.WrapLayout;
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
 public class JPanelCustom extends JPanel {
 
-    private PanelType defaultPanel  = PanelType.BORDER;
+    public static final int MAX_INT = Integer.MAX_VALUE;
+    public static final int ZERO_INT = 0;
+    public static final Dimension ZERO = new Dimension(ZERO_INT, ZERO_INT);
+    public static final Dimension MAX = new Dimension(MAX_INT, MAX_INT);
 
-    private String defaultIconPath = "anythingPathToIcon";
-
-    public enum PanelType {
-        GRID, BORDER, FLOW, CARD, BOX, WRAP
-    }
-
-
-    // Окно для отображения перетаскиваемого объекта (визуальный эффект во время drag)
-    //private final JWindow cursorWindow = new JWindow();
+    private static final PanelType DEFAULT_TYPE = PanelType.BORDER;
 
     // Конструкторы
-    public JPanelCustom(PanelType type, boolean clearBorder) {
-        this(type);
-        handleClearBorder(clearBorder);
-    }
-
-    // Конструкторы
-    public JPanelCustom(boolean clearBorder) {
-        this();
-        handleClearBorder(clearBorder);
-    }
-
-    public JPanelCustom(PanelType type, String axis) {
-        handleAXIS(type, axis);
-    }
-
-    public JPanelCustom(String axis) {
-        handleAXIS(defaultPanel, axis);
-    }
-
-    public JPanelCustom(PanelType type, String axis, int a, int b) {
-        this(type);
-        handleAXIS(type, axis, a, b);
-    }
-
-
-    public JPanelCustom() {
-        setLayoutByType(defaultPanel);
-    }
-
-    public JPanelCustom(PanelType type) {
-        setLayoutByType(type);
-        // В базовом классе drop-логика не инициализируется автоматически!
-        // Для включения drop нужно явно вызвать setupDropTarget() в подклассе.         актуально ли я не помню
-    }
-
-    public JPanelCustom(LayoutManager layout) {
-        super(layout);
-        // Не инициализируем drop-логику автоматически
-    }
-
-    // Установка layout в зависимости от типа панели
-    private void setLayoutByType(PanelType type) {
-        switch (type) {
-            case GRID:
-                setLayout(new GridBagLayout());
-                break;
-            case BORDER:
-                setLayout(new BorderLayout());
-                break;
-            case FLOW:
-                setLayout(new FlowLayout());
-                break;
-            case CARD:
-                setLayout(new CardLayout());
-                break;
-            case BOX:
-                setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-                break;
-            case WRAP:
-                setLayout(new WrapLayout());
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown panel type: " + type);
-        }
-    }
-
-    // Настройка оси для BoxLayout и FlowLayout
-    private void handleAXIS(PanelType type, String axis, int a, int b) {
-        if (type.equals(PanelType.FLOW)) {
-            switch (axis) {
-                case "LEFT":
-                    setLayout(new FlowLayout(FlowLayout.LEFT, a, b));
-                    break;
-                case "RIGHT":
-                    setLayout(new FlowLayout(FlowLayout.RIGHT, a, b));
-                    break;
-                case "LEADING":
-                    setLayout(new FlowLayout(FlowLayout.LEADING, a, b));
-                    break;
-                case "CENTER":
-                    setLayout(new FlowLayout(FlowLayout.CENTER, a, b));
-                    break;
-                case "TRAILING":
-                    setLayout(new FlowLayout(FlowLayout.TRAILING, a, b));
-                    break;
-                default:
-                    System.out.println("Unsupported axis: " + axis);
-            }
-        } else {
-            System.out.println("The layout is not of border type");
-        }
-    }
-
-    public void setAXIS(PanelType type, String axis) {
-        handleAXIS(type, axis);
-    }
-
-    // Настройка оси для BoxLayout и FlowLayout
-    private void handleAXIS(PanelType type, String axis) {
-        if (type.equals(PanelType.BORDER)) {
-            switch (axis) {
-                case "X":
-                    setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-                    break;
-                case "PAGE":
-                    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-                    break;
-                case "LINE":
-                    setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
-                    break;
-                case "Y":
-                    setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-                    break;
-                default:
-                    System.out.println("Unsupported axis: " + axis);
-            }
-        } else if (type.equals(PanelType.FLOW)) {
-            switch (axis) {
-                case "LEFT":
-                    setLayout(new FlowLayout(FlowLayout.LEFT));
-                    break;
-                case "RIGHT":
-                    setLayout(new FlowLayout(FlowLayout.RIGHT));
-                    break;
-                case "LEADING":
-                    setLayout(new FlowLayout(FlowLayout.LEADING));
-                    break;
-                case "CENTER":
-                    setLayout(new FlowLayout(FlowLayout.CENTER));
-                    break;
-                case "TRAILING":
-                    setLayout(new FlowLayout(FlowLayout.TRAILING));
-                    break;
-                default:
-                    System.out.println("Unsupported axis: " + axis);
-            }
-        } else if (type.equals(PanelType.WRAP)) {
-            switch (axis) {
-                case "LEFT":
-                    setLayout(new WrapLayout(FlowLayout.LEFT));
-                    break;
-                case "RIGHT":
-                    setLayout(new WrapLayout(FlowLayout.RIGHT));
-                    break;
-                case "LEADING":
-                    setLayout(new WrapLayout(FlowLayout.LEADING));
-                    break;
-                case "CENTER":
-                    setLayout(new WrapLayout(FlowLayout.CENTER));
-                    break;
-                case "TRAILING":
-                    setLayout(new WrapLayout(FlowLayout.TRAILING));
-                    break;
-                default:
-                    System.out.println("Unsupported axis: " + axis);
-            }
-        } else if (type.equals(PanelType.BOX)) {
-            switch (axis) {
-                case "PAGE":
-                    setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-                    break;
-                case "LINE":
-                    setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
-                    break;
-                case "X":
-                    setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-                    break;
-                case "Y":
-                    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-                    break;
-                default:
-                    System.out.println("Unsupported axis: " + axis);
-            }
-        } else {
-            System.out.println("The layout is not of border type");
-        }
-    }
-
-    private void handleClearBorder(boolean clearBorder) {
+    public JPanelCustom(PanelType type, Axis axis, int hgap, int vgap, boolean clearBorder) {
+        setLayout(type.createLayout(this, axis, hgap, vgap));
         if (clearBorder) {
-            setPreferredSize(new Dimension(0, 0));
-            setMinimumSize(new Dimension(0, 0));
+            setPreferredSize(ZERO);
+            setMinimumSize(ZERO);
         }
     }
 
-    // Фабричные методы для создания панелей
-    public static JPanelCustom createGridPanel(boolean clearBorder) {
-        return new JPanelCustom(PanelType.GRID, clearBorder);
+    /*** Конструктор по умолчанию.*/
+    public JPanelCustom() {
+        this(DEFAULT_TYPE, null, 0, 0, false);
     }
 
-    public static JPanelCustom createBorderPanel(boolean clearBorder) {
-        return new JPanelCustom(PanelType.BORDER, clearBorder);
+    /*** Конструктор установки нулевых границ.*/
+    public JPanelCustom(boolean clearBorder) {
+        this(DEFAULT_TYPE, null, 0, 0, clearBorder);
     }
 
-    public static JPanelCustom createFlowPanel(boolean clearBorder) {
-        return new JPanelCustom(PanelType.FLOW, clearBorder);
+    /*** Конструктор установки нулевых границ с типом панели.*/
+    public JPanelCustom(PanelType type, boolean clearBorder) {
+        this(type, null, 0, 0, clearBorder);
     }
 
-    // Прочие методы для работы с layout (например, GridBagLayout)
-    public void setGridBagConstrains(JPanelCustom panel1, JPanelCustom panel2) {
-        if (!getLayout().getClass().getName().contains("GridBagLayout")) {
-            System.out.println("Незаконная попытка установить GridBagConstraints");
-            return;
-        }
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.weightx = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.anchor = GridBagConstraints.NORTH;
-        gbc.weighty = 0;
-        gbc.gridy = 0;
-        this.add(panel1, gbc);
-        gbc.gridy = 1;
-        this.add(panel2, gbc);
-        gbc.gridy = 2;
-        gbc.weighty = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        this.add(Box.createGlue(), gbc);
+    /*** Упрощённый: без отступов.*/
+    public JPanelCustom(PanelType type, Axis axis) {
+        this(type, axis, 0, 0, false);
     }
 
-    public GridBagConstraints menuGridBagConstraintsX(int gridX, double weightX) {
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = gridX;
-        gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
-        gbc.weightx = weightX;
-        gbc.weighty = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        return gbc;
+    /*** Упрощённый: только ось, тип панели по умолчанию.*/
+    public JPanelCustom(Axis axis) {
+        this(DEFAULT_TYPE, axis, 0, 0, false);
+    }
+
+    /*** Базовый конструктор по типу без осей и отступов.*/
+    public JPanelCustom(PanelType type) {
+        setLayout(type.createLayout(this, null, 0, 0));
+    }
+
+
+    public void setDimensions(Dimension minimum, Dimension preferred, Dimension maximum) {
+        setMinimumSize(minimum);
+        setPreferredSize(preferred);
+        setMinimumSize(maximum);
+    }
+
+    public void setPreferredSize(int x, int y) {
+        super.setPreferredSize(new Dimension(x, y));
+    }
+
+    public void setMaximumSize(int x, int y) {
+        super.setMaximumSize(new Dimension(x, y));
+    }
+
+    public void setMinimumSize(int x, int y) {
+        super.setMinimumSize(new Dimension(x, y));
     }
 
 
---
+++ src/main/java/swing/objects/general/Pages.java
@@ -0,0 +1,50 @@
+package swing.objects.general;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Pages extends JPanelCustom {
+
+    public Pages(PanelType type, boolean clearBorder) {
+        super(type, clearBorder);
+    }
+
+    public Pages(PanelType type) {
+        super(type);
+    }
+
+    //Grid
+    //todo Переработать системы с Grid панелями
+    public void setGridBagConstrains(JPanelCustom panel1, JPanelCustom panel2) {
+        if (!getLayout().getClass().getName().contains("GridBagLayout")) {
+            System.out.println("Незаконная попытка установить GridBagConstraints");
+            return;
+        }
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.weightx = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.weighty = 0;
+        gbc.gridy = 0;
+        this.add(panel1, gbc);
+        gbc.gridy = 1;
+        this.add(panel2, gbc);
+        gbc.gridy = 2;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        this.add(Box.createGlue(), gbc);
+    }
+
+    public GridBagConstraints menuGridBagConstraintsX(int gridX, double weightX) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = gridX;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = weightX;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        return gbc;
+    }
+}
