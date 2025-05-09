b8bdb646cb2d33e7485a4cf4c9756db7beb1505d
meowmeow <47986831+true-meowmeow@users.noreply.github.com>
2025-05-09 20:09:32 +0400

    Код, как вода – миг, и утекает в никуда!
    Signed-off-by: meowmeow <47986831+true-meowmeow@users.noreply.github.com>

--- src/main/java/swing/ui/pages/home/SwingHomeVariables.java
+++ src/main/java/swing/ui/pages/home/SwingHomeVariables.java
@@ -1,8 +1,16 @@
 package swing.ui.pages.home;
 
+import java.awt.*;
+
 public interface SwingHomeVariables {
 
     int HEIGHT_COLLECTION_PANEL = 80;
     int HEIGHT_ADD_COLLECTION_PANEL = 80;
-    int HEIGHT_POPUP_ADD_COLLECTION_PANEL = 80;
+    //int HEIGHT_POPUP_ADD_COLLECTION_PANEL = 80;
+
+
+    Color basicColor = new Color(0xF5F5F5);
+    Color aOpenedColor = new Color(0xDCDCDC);
+    Color aDragColor = new Color(0xFFC6C6C6, true);
+    Color aFocusColor = new Color(0xE0E0E0);
 }
--- src/main/java/swing/ui/pages/home/collections/droppers/DropTargetNewCollection.java
+++ src/main/java/swing/ui/pages/home/collections/droppers/DropTargetNewCollection.java
@@ -1,11 +1,22 @@
 package swing.ui.pages.home.collections.droppers;
 
 import swing.objects.dropper.DropPanelAbstract;
+import swing.ui.pages.home.play.view.selection.SelectablePanel;
+
+import java.util.List;
 
 public class DropTargetNewCollection extends DropPanelAbstract {
     @Override
     public void dropAction() {
         System.out.println("«Добавить новую коллекцию»");
         // TODO: вызвать отображение вашей временной панели
+
+    }
+
+    @Override
+    public void dropAction(List<SelectablePanel> selectedItems) {
+        super.dropAction(selectedItems);
+
+        System.out.println(selectedItems.size());
     }
 }
