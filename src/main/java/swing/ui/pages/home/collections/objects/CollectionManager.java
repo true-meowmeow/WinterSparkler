dc947dc4d151f4b72d9ea9e395a13692250f48b2
meowmeow <47986831+true-meowmeow@users.noreply.github.com>
2025-05-13 06:36:44 +0400

    Ветку не роняй, самурай!

---
+++ src/main/java/swing/ui/pages/home/collections/objects/CollectionManager.java
@@ -0,0 +1,71 @@
+package swing.ui.pages.home.collections.objects;
+
+import core.contentManager.CollectionNameGenerator;
+import core.contentManager.MediaData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionManager {
+    private final List<CollectionObject> objects = new ArrayList<>();
+    String nameGenerator;
+
+    private final CollectionObjectPanel collectionObjectPanel;
+
+    public CollectionManager(CollectionObjectPanel collectionObjectPanel) {
+        this.collectionObjectPanel = collectionObjectPanel;
+    }
+
+    OrderCounter counter;
+    CollectionNameGenerator collectionNameGenerator;
+    public void addCollection(List<MediaData> mediaGroupList) {
+        counter = new OrderCounter();
+        //collectionNameGenerator = new CollectionNameGenerator();
+
+        int collectionOrderNumber = counter.getNextNumber();
+        nameGenerator = "123";
+
+        CollectionObject collectionObject = new CollectionObject(getPropertyName(collectionOrderNumber), collectionOrderNumber, objects.size(), nameGenerator);
+        objects.add(collectionObject);
+
+        collectionObjectPanel.refreshUI();
+    }
+
+
+    private String getPropertyName(int collectionOrder) {
+        return "_COLLECTION_OBJECT_" + collectionOrder + "_";
+    }
+
+    public void removeObject(int positionList) {
+        if (positionList < 0 || positionList >= objects.size()) return;
+        objects.remove(positionList);
+        collectionObjectPanel.resequence();
+        collectionObjectPanel.refreshUI();
+    }
+
+    public void renameObject(int positionList, String newTitle) {
+        if (positionList < 0 || positionList >= objects.size()) return;
+        objects.get(positionList).setTitle(newTitle);
+        collectionObjectPanel.refreshUI();
+    }
+
+    public List<CollectionObject> getObjects() {
+        return objects;
+    }
+
+    private class OrderCounter {
+        private int current;
+
+        public OrderCounter() {
+            this(1);
+        }
+
+        public OrderCounter(int start) {
+            current = start - 1;
+        }
+
+        public int getNextNumber() {
+            return ++current;
+        }
+    }
+}
